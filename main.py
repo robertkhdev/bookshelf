83b0c7b6b4fa6f1397c6ada10c560d191427ad68
Robert Hammond <robertkh@gmail.com>
2020-09-16 20:23:37 -0500

    formatting item rows

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import PySimpleGUI as sg
 import json
 import amazon
 import pathlib
-from typing import Dict, List
+from typing import Any, Dict, List
 
 
 def load_list(file_name: str) -> List[Dict[str, str]]:
@@ -11,10 +11,16 @@ def load_list(file_name: str) -> List[Dict[str, str]]:
         return json.loads(f.read())
 
 
-def make_detail_line(item):
+def make_detail_line(item: Any) -> str:
     text = 'Price: ' + item['price_amazon'] + ' Used&New: ' + item['price_used_new'] + \
-           'Avg. Rating: ' + str(item['rating'][0]) + '/' + str(item['rating'][1]) + ' Reviews: ' + item['num_reviews']
+           'Avg. Rating: ' + str(item['rating'][0]) + '/' + str(item['rating'][1]) + \
+           ' Reviews: ' + item['num_reviews']
     return text
+
+
+def make_item_row(item: Any) -> str:
+    row = [sg.Text(item['name'] + '\n' + item['by_line'] + '\n' + make_detail_line(item))]
+    return row
 
 
 def main_window():
@@ -28,8 +34,10 @@ def main_window():
     items = load_list('listdump.json')
     # rows = [[sg.Checkbox(item['name'])] for item in items]
 
-    rows = [[sg.Frame(layout=[[sg.Text(item['name'])], [sg.Text(item['by_line'])],
-                              [sg.Text(make_detail_line(item))]], title='')] for item in items]
+    # rows = [[sg.Frame(layout=[[sg.Text(item['name'])], [sg.Text(item['by_line'])],
+    #                           [sg.Text(make_detail_line(item))]], title='')] for item in items]
+
+    rows = [make_item_row(item) for item in items]
 
     layout = [[sg.Text('TEST')],
               [sg.Text('Input Text: ')], [sg.InputText()],
