ef0f7a904aeb1b6edac0c78b54961dc5c2a9b8fa
Robert Hammond <robertkh@gmail.com>
2020-11-24 20:11:28 -0600

    remove temp test code

--- listutils.py
+++ listutils.py
@@ -90,8 +90,4 @@ def load_list(file_name: str) -> List[Dict[str, str]]:
 
 
 if __name__ == '__main__':
-    # test list retrieval
-    
-    items_test = amazon.get_amazon_list(url)
-    print([amazon.parse_item_name(i) for i in items_test])
-    save_list(items_test)
+    pass
